7b7837e31e7bec566aeee545f20f7a8603dbd983
EthanChe <99823373+EthanChe@users.noreply.github.com>
2022-04-20 21:52:34 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const eventRoutes = require('./events');
+const eventRoutes = require('./login');
 
 const constructorMethod = (app) => {
   app.use('/events', eventRoutes);
+  app.use('/login', loginRoutes);
 
   app.use('*', (req, res) => {
     res.status(404).json({ error: 'Not found' });
