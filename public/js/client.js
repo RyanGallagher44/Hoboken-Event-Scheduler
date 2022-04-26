afa3e655f3a041566be1ecaf1f6acabd91abebb8
Ethan Che <eche@stevens.edu>
2022-04-25 21:07:27 -0400

    client side form validation

--- public/js/client.js
+++ public/js/client.js
@@ -36,4 +36,48 @@
             $('html').html(responseMessage);
         });
     });
+
+    //Validate event comment submission before sending to route
+    var eventComment = $('#add_comment');
+    $(eventComment).on('submit', function(e) {
+        e.preventDefault();
+        var comment = $('#comment_text').val();
+        if (!comment || comment.trim().length == 0) {
+            // empty comment :(
+        }
+        else {
+            $(this).unbind();
+            $(this).submit();
+        }
+    });
+
+    //Validate search by event term
+    var searchEvent = $('#search_event');
+    $(searchEvent).on('submit', function(e) {
+        e.preventDefault();
+        var term = $('#search').val();
+        if (!term || term.trim().length == 0) {
+            // empty search term :(
+        }
+        else {
+            $(this).unbind();
+            $(this).submit();
+        }
+    });
+
+    //Validate login form
+    var loginForm = $('#login-form');
+    $(loginForm).on('submit', function(e) {
+        e.preventDefault();
+        var email = $('#login-email').val();
+        var password = $('#login-password').val();
+        if (!email || email.trim().length == 0 || !password || password.trim().length == 0) {
+            // empty email or password :(
+            //Google (and by that I mean stackoverflow) says to not validate email address clientside. So we only check if something is present here
+        }
+        else {
+            $(this).unbind();
+            $(this).submit();
+        }
+    });
 })(window.jQuery);
