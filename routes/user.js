d73726c42a3fa7606767cf4f4edf989621e4c72f
RyanGallagher44 <rgallag1@stevens.edu>
2022-05-06 00:38:03 -0400

    You can now follow/unfollow other profiles, and you can view your following/followers

--- routes/user.js
+++ routes/user.js
@@ -37,6 +37,37 @@ router.get('/regEvents', async (req, res) => {
     res.render("shows/registeredEvents", {title: "My Registered Events", name: `${userFirst} ${userLast}`, events: evList, loggedIn: true});
 });
 
+router.get('/followers', async (req, res) => {
+    try {
+        res.render('shows/followers', {title: "Followers", loggedIn: true, name: `${(await userData.get(req.session.userId)).firstName} ${(await userData.get(req.session.userId)).lastName}`, followers: (await userData.getFollowers(req.session.userId))});
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.get('/following', async (req, res) => {
+    try {
+        console.log(await userData.getFollowing(req.session.userId));
+        res.render('shows/following', {title: "Following", loggedIn: true, name: `${(await userData.get(req.session.userId)).firstName} ${(await userData.get(req.session.userId)).lastName}`, following: (await userData.getFollowing(req.session.userId))});
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.post('/follow', async (req, res) => {
+    await userData.addToFollowing(req.body.userToFollowId, req.session.userId);
+    await userData.addToFollowers(req.body.userToFollowId, req.session.userId);
+
+    res.redirect(`/user/${req.body.userToFollowId}`);
+});
+
+router.post('/unfollow', async (req, res) => {
+    await userData.removeFromFollowing(req.body.userToUnfollowId, req.session.userId);
+    await userData.removeFromFollowers(req.body.userToUnfollowId, req.session.userId);
+
+    res.redirect(`/user/${req.body.userToUnfollowId}`);
+});
+
 router.get('/:id', async (req, res) => {
     if (req.params.id == req.session.userId) {
         return res.redirect('/user');
@@ -46,7 +77,11 @@ router.get('/:id', async (req, res) => {
         const pastAttended = await userData.getPastAttendedEvents(req.params.id);
         req.session.prevURL = '/user/other';
         req.session.prevVisitedProfile = req.params.id;
-        res.render('shows/user_profile', {title: "Your Profile", loggedIn: true, name: `${(await userData.get(req.session.userId)).firstName} ${(await userData.get(req.session.userId)).lastName}`, profile_name: `${(await userData.get(req.params.id)).firstName} ${(await userData.get(req.params.id)).lastName}`, age: `${(await userData.get(req.params.id)).age}`, email: `${(await userData.get(req.params.id)).email}`, events_hosted: pastHosted, events_attended: pastAttended, otherUser: true})
+        if (((await userData.get(req.session.userId)).following).includes(req.params.id)) {
+            res.render('shows/user_profile', {title: "Your Profile", loggedIn: true, name: `${(await userData.get(req.session.userId)).firstName} ${(await userData.get(req.session.userId)).lastName}`, profile_name: `${(await userData.get(req.params.id)).firstName} ${(await userData.get(req.params.id)).lastName}`, age: `${(await userData.get(req.params.id)).age}`, email: `${(await userData.get(req.params.id)).email}`, events_hosted: pastHosted, events_attended: pastAttended, otherUser: true, user_id: req.params.id, alreadyFollowing: true});
+        } else {
+            res.render('shows/user_profile', {title: "Your Profile", loggedIn: true, name: `${(await userData.get(req.session.userId)).firstName} ${(await userData.get(req.session.userId)).lastName}`, profile_name: `${(await userData.get(req.params.id)).firstName} ${(await userData.get(req.params.id)).lastName}`, age: `${(await userData.get(req.params.id)).age}`, email: `${(await userData.get(req.params.id)).email}`, events_hosted: pastHosted, events_attended: pastAttended, otherUser: true, user_id: req.params.id});
+        }
     } catch (e) {
         console.log(e);
     }
