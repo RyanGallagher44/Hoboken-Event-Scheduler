ed2b73d0c8a2d2da85a28144c7e92bbbe13053ae
Ethan Che <eche@stevens.edu>
2022-05-08 14:01:32 -0400

    variable validation

--- routes/landing.js
+++ routes/landing.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const data = require('../data');
 const userData = data.users;
 const xss = require('xss');
+const validation = require('../validation');
 
 router.get('/', async (req, res) => {
-    let userId = xss(req.session.userId);
+    let userId = validation.checkId(xss(req.session.userId), "user ID");
     if (userId) {
         const user = await userData.get(userId);
         res.render('shows/all_events', {title: "All Events", loggedIn: true, name: `${user.firstName} ${user.lastName}`});
